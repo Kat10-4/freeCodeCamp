97ab6c6d27b9f1ce37fc128eb5a245d75b00ce17
Kateryna Diachenko <143129834+Kat10-4@users.noreply.github.com>
2024-01-02 20:09:17 +0200

    js: goFight func

--- basik JS/script.js
+++ basik JS/script.js
@@ -129,6 +129,10 @@ function sellWeapon() {
 
 function goFight() {
   update(locations[3]);
+  monsterHealth = monsters[fighting].health;
+  monsterStats.style.display = "block";
+  monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 }
 
 function fightDragon() {
