dc902cbb137e68b60f0b440411926733ac637163
Kateryna Diachenko <143129834+Kat10-4@users.noreply.github.com>
2024-02-14 22:46:49 +0200

    count calories

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -46,6 +46,29 @@ function getCaloriesFromInputs(list) {
   return calories;
 }
 
-function calculateCalories(e) {}
+function calculateCalories(e) {
+  e.preventDefault();
+  isError = false;
+  const breakfastNumberInputs = document.querySelectorAll(
+    "#breakfast input[type=number]"
+  );
+  const lunchNumberInputs = document.querySelectorAll(
+    "#lunch input[type=number]"
+  );
+  const dinnerNumberInputs = document.querySelectorAll(
+    "#dinner input[type=number]"
+  );
+  const snacksNumberInputs = document.querySelectorAll(
+    "#snacks input[type=number]"
+  );
+  const exerciseNumberInputs = document.querySelectorAll(
+    "#exercise input[type=number]"
+  );
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs);
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+}
 
 addEntryButton.addEventListener("click", addEntry);
