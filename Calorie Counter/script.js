cd1be1d9a6439b06e12ab848da5e3a73c68fad96
Kateryna Diachenko <143129834+Kat10-4@users.noreply.github.com>
2024-02-14 19:35:27 +0200

    update getCaloriesFromInputs

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -31,6 +31,17 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML("beforeend", HTMLString);
 }
 
-function getCaloriesFromInputs(list) {}
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+  for (const item of list) {
+    const currVal = cleanInputString(item.value);
+    const invalidInputMatch = isInvalidInput(currVal);
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+  }
+}
 
 addEntryButton.addEventListener("click", addEntry);
