15b2745c063f4e227f4554ccf50334b07bbad935
Kateryna Diachenko <143129834+Kat10-4@users.noreply.github.com>
2024-02-15 20:33:37 +0200

    update calculateCalories

--- Calorie Counter/script.js
+++ Calorie Counter/script.js
@@ -69,6 +69,18 @@ function calculateCalories(e) {
   const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
   const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
   const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]);
+  if (isError) {
+    return;
+  }
+  const consumedCalories =
+    breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+  output.innerHTML = `<span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(
+    remainingCalories
+  )} Calorie ${surplusOrDeficit}</span>`;
 }
 
 addEntryButton.addEventListener("click", addEntry);
